43032ea265bed8ccd57769dbd7a412c3ce434521
David Conley <dconley1989@gmail.com>
2022-09-04 15:42:05 -0600

    added my second test to check if the dom was loaded properly with the right elements

---
+++ future-currency/src/_tests/App.test.js
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import App from "../App";
+
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("form, file input, text area, and submit button are in the DOM", () => {
+  render(<App />);
+
+  const form = screen.getByRole("form");
+  const inputFile = screen.getByLabelText("Upload your data");
+  const textArea = screen.getByLabelText("Add your data below");
+  const button = screen.getByRole("button", { name: "Submit" });
+
+  expect(form).toBeInTheDocument();
+  expect(inputFile).toBeInTheDocument();
+  expect(textArea).toBeInTheDocument();
+  expect(button).toBeInTheDocument();
+});
