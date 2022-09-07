641718b68517affb80411f2b18e4bcfa35b724d3
David Conley <dconley1989@gmail.com>
2022-09-07 14:14:10 -0600

    added styling and fixed some tests that were broken due to the styleing components library that I added

--- future-currency/src/App.js
+++ future-currency/src/App.js
@@ -1,29 +1,65 @@
 import "./App.css";
 import React, { useState } from "react";
 import axios from "axios";
+import styled from "styled-components";
 
-/* left off testing how the data might look when a user adds it
-so that I can make a mock test of how I want to format the data
-before sending to the api
+/* I used the styling components library to grab the appriopriate
+element and add the styling I created for it
 */
 
+const StyledAppWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #85bb65;
+  height: 100vh;
+`;
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #f5f5f5;
+  border-radius: 10px;
+  width: 50%;
+  padding: 3rem 0rem;
+  border: 1px solid #000000;
+  margin-top: 3rem;
+`;
+const StyledLabel = styled.label`
+  font-size: 1rem;
+  margin: 1rem auto;
+  font-weight: bold;
+`;
+const StyledInput = styled.input`
+  margin-bottom: 1.5rem;
+`;
+const StyledTextArea = styled.textarea`
+  padding: 3rem;
+  margin-bottom: 1rem;
+`;
+const StyledButton = styled.button`
+  padding: 1em 2em;
+  width: 40%;
+  border-radius: 10px;
+  background-color: #85bb65;
+  font-size: 1em;
+`;
+
 function App() {
+  //Handling all the state that the user inputs whether throught the file upload
+  // or the text area.
   const [textAreaObject, setTextAreaObject] = useState({
     textArea: "",
   });
+  const [file, setFile] = useState(null);
+  //handling the state that comes from the response of the api call
+  const [maxProfit, setMaxProfit] = useState(null);
 
-  let fileInForm = new FormData();
-
+  /* the next two arrow functions handle the user actions when
+   they upload a file or add to the text area which then is added
+  to the state */
   const handleFileUpload = (e) => {
-    const file = e.target.files[0];
-    fileInForm.append("file", file);
-    // const reader = new FileReader();
-    // reader.onload = function (e) {
-    //   console.log(e.target.result);
-    // };
-    // const fileAsText = reader.readAsText(file);
-    // console.log(fileAsText);
-    // console.log(formObject.file);
+    setFile(e.target.files[0]);
   };
   const handleTextArea = (e) => {
     const arrayData = e.target.value.split(/\n/);
@@ -31,41 +67,71 @@ function App() {
     console.log(arrayData);
     setTextAreaObject({ [e.target.name]: arrayData });
   };
-
+  /* the arrow functin below handles the user clicking on the submit button
+and sending a request to the api that should handle the request appriopriately
+and send back a response but I couldn't handle the data the way I wanted to
+for the text area. I kept getting a failure for the file axios call because of 
+the information on my header not matching the multer requirements for the backend.
+Additionally, I don't know if I was using the correct file format. Because I was just
+trying to use a text file. It could have been necessary to use a .csv
+*/
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (textAreaObject.textArea.length === 0) {
+    let fileInForm = new FormData();
+    fileInForm.append("file", file);
+    if (file !== null) {
       axios
-        .post("http://localhost:9000/price/difference", fileInForm)
-        .then((res) => console.log(res))
+        .post("http://localhost:9000/file", fileInForm)
+        .then((res) => {
+          console.log(res);
+          setMaxProfit(res.data);
+        })
         .catch((err) => console.log(err));
     } else {
       axios
-        .post("http://localhost:9000/price/difference", textAreaObject)
-        .then((res) => console.log(res))
+        .post("http://localhost:9000/textArea", textAreaObject)
+        .then((res) => {
+          console.log(res);
+          setMaxProfit(res.data);
+        })
         .catch((err) => console.log(err));
     }
   };
   return (
-    <div className="App">
-      <form aria-label="currency info" onSubmit={handleSubmit}>
-        <label>
-          Upload your data
-          <input name="file" type="file" onChange={handleFileUpload} />
-        </label>
-        <label id="textArea">
-          Add your data below
-          <textarea
-            id="textArea"
-            name="textArea"
-            onChange={handleTextArea}
-            value={textAreaObject.textArea}
-          ></textarea>
-        </label>
-        <button>Submit</button>
-      </form>
-      {}
-    </div>
+    <StyledAppWrapper className="App">
+      <h1>Finding Max Profit!</h1>
+      <StyledForm
+        encType="multipart/form-data"
+        aria-label="currency info"
+        onSubmit={handleSubmit}
+        data-testid="form"
+      >
+        <StyledLabel>Upload your data</StyledLabel>
+        <StyledInput
+          name="uploaded_file"
+          type="file"
+          onChange={handleFileUpload}
+          data-testid="Upload Data"
+        />
+        <StyledLabel id="textArea">Add your data below</StyledLabel>
+        <StyledTextArea
+          data-testid="Add Data"
+          id="textArea"
+          name="textArea"
+          onChange={handleTextArea}
+          value={textAreaObject.textArea}
+        ></StyledTextArea>
+        <StyledButton>Submit</StyledButton>
+      </StyledForm>
+      {maxProfit !== null ? (
+        <div>
+          <h2>Buy low and sell high data:</h2>
+          {Object.entries(maxProfit).map(([key, value]) => {
+            return <p key={key}>{`${key}: ${JSON.stringify(value)}`}</p>;
+          })}
+        </div>
+      ) : null}
+    </StyledAppWrapper>
   );
 }
 
--- future-currency/src/_tests/App.test.js
+++ future-currency/src/_tests/App.test.js
@@ -1,5 +1,11 @@
 import { render, screen, fireEvent } from "@testing-library/react";
-import App from "../App";
+import App,  from "../App";
+
+/* I added a few tests for what I was trying to accomplish but
+ultimately found that TDD was harder than I imagined because I would
+try and write the test but I am a visually person so I need to get in a 
+better habit of writing tests before manually testing.
+*/
 
 test("renders app", () => {
   render(<App />);
@@ -8,10 +14,10 @@ test("renders app", () => {
 test("form, file input, text area, and submit button are in the DOM", () => {
   render(<App />);
 
-  const form = screen.getByRole("form", { name: /currency info/i });
-  const inputFile = screen.getByLabelText("Upload your data");
-  const textArea = screen.getByLabelText("Add your data below");
-  const button = screen.getByRole("button", { name: "Submit" });
+  const form = screen.getByTestId("form");
+  const inputFile = screen.getByTestId("Upload Data");
+  const textArea = screen.getByTestId("Add Data");
+  const button = screen.getByText("Submit");
 
   expect(form).toBeInTheDocument();
   expect(inputFile).toBeInTheDocument();
@@ -22,8 +28,10 @@ test("form, file input, text area, and submit button are in the DOM", () => {
 test("testing textarea has a value when changed", () => {
   render(<App />);
 
-  const textArea = screen.getByLabelText("Add your data below");
+  const textArea = screen.getByTestId("Add Data");
   fireEvent.change(textArea, { target: { value: "2222200533351113001100" } });
 
   expect(textArea.value).toBe("2222200533351113001100");
 });
+
+test("testing asynchronous call", () => {});
